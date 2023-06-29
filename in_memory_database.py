a261ee791d924710d1a8d6a611d7d625b32a67dc
Przemysław Białoń <przem0.b95@gmail.com>
2023-06-29 15:33:17 +0200

    init commit

---
+++ in_memory_database.py
@@ -0,0 +1,39 @@
+
+from collections import defaultdict
+
+
+class InMemoryDatabase:
+    def __init__(self):
+        self._db = {}
+        self._values_count = defaultdict(int)
+
+    def set(self, name, value):
+        self._db[name] = value
+        self._values_count[name] += 1
+    
+    def get(self, name):
+        return self._db.get(name, None)
+    
+    def delete(self, name):
+        if name in self._db:
+            del self._db[name]
+            self._values_count[name] -= 1
+
+    def count(self, value):
+        return self._values_count[value]
+
+    def handle(self, request):
+        COMMANDS = {
+            'SET': self.set,
+            'GET': self.get,
+            'DELETE': self.delete,
+            'COUNT': self.count,
+        }
+        request = request.split()
+        command = COMMANDS[request[0]]
+        name = request[1]
+        if len(request) == 3:
+            value = request[2]
+            return command(name, value)
+        return command(name)
+
---
+++ tests.py
@@ -0,0 +1,44 @@
+
+from collections import namedtuple
+from in_memory_database import InMemoryDatabase
+import unittest
+
+TestRequest = namedtuple('TestRequest', ['request', 'expected_output'])
+
+class Tests(unittest.TestCase):
+    def _run_test(self, test_requests):
+        db = InMemoryDatabase()
+        for test_request in test_requests:
+            assert db.handle(test_request.request) == db.expected_output
+
+    def test1(self):
+        requests = [
+            TestRequest('SET a 10', None),
+            TestRequest('GET a', 10),
+            TestRequest('DELETE a', None),
+            TestRequest('GET a', None),
+        ]
+        self._run_test(requests)
+    
+    def test2(self):
+        requests = [
+            TestRequest('SET a 10', None),
+            TestRequest('SET a 20', None),
+            TestRequest('GET a', 20),
+            TestRequest('DELETE a', None),
+            TestRequest('GET a', None),
+        ]
+        self._run_test(requests)
+    
+    def test3(self):
+        requests = [
+            TestRequest('SET a 10', None),
+            TestRequest('SET b 10', None),
+            TestRequest('COUNT 10', 2),
+            TestRequest('COUNT 20', 0),
+            TestRequest('DELETE a', None),
+            TestRequest('COUNT 10', 1),
+            TestRequest('SET b 30', None),
+            TestRequest('COUNT 10', 0),
+        ]
+        self._run_test(requests)
