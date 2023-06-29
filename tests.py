837ac5fd31ab148bcce299aadb7887d6805b8e7d
Przemysław Białoń <przem0.b95@gmail.com>
2023-06-29 15:52:46 +0200

    added tests for transactions

--- tests.py
+++ tests.py
@@ -11,7 +11,7 @@ class Tests(unittest.TestCase):
         for i, test_request in enumerate(test_requests):
             self.assertEqual(db.handle(test_request.request), test_request.expected_output, f"request-{i + 1}: {test_request}")
 
-    def test1(self):
+    def test_standard1(self):
         requests = [
             TestRequest('SET a 10', None),
             TestRequest('GET a', '10'),
@@ -20,7 +20,7 @@ class Tests(unittest.TestCase):
         ]
         self._run_test(requests)
     
-    def test2(self):
+    def test_standard2(self):
         requests = [
             TestRequest('SET a 10', None),
             TestRequest('SET a 20', None),
@@ -30,7 +30,7 @@ class Tests(unittest.TestCase):
         ]
         self._run_test(requests)
     
-    def test3(self):
+    def test_standard3(self):
         requests = [
             TestRequest('SET a 10', None),
             TestRequest('SET b 10', None),
@@ -42,3 +42,62 @@ class Tests(unittest.TestCase):
             TestRequest('COUNT 10', 0),
         ]
         self._run_test(requests)
+
+    def test_transactions1(self):
+        requests = [
+            TestRequest('BEGIN', None),
+            TestRequest('SET a 10', None),
+            TestRequest('GET a', '10'),
+            TestRequest('BEGIN', None),
+            TestRequest('SET a 20', None),
+            TestRequest('GET a', '20'),
+            TestRequest('ROLLBACK', None),
+            TestRequest('GET a', '10'),
+            TestRequest('ROLLBACK', None),
+            TestRequest('GET a', None),
+        ]
+
+        self._run_test(requests)
+
+    def test_transactions2(self):
+        requests = [
+            TestRequest('BEGIN', None),
+            TestRequest('SET a 30', None),
+            TestRequest('BEGIN', None),
+            TestRequest('SET a 40', None),
+            TestRequest('COMMIT', None),
+            TestRequest('GET a', '40'),
+            TestRequest('ROLLBACK', None),
+        ]
+
+        self._run_test(requests)
+
+    def test_transactions3(self):
+        requests = [
+            TestRequest('SET a 50', None),
+            TestRequest('BEGIN', None),
+            TestRequest('GET a', '50'),
+            TestRequest('SET a 60', None),
+            TestRequest('BEGIN', None),
+            TestRequest('DELETE a', None),
+            TestRequest('GET a', None),
+            TestRequest('ROLLBACK', None),
+            TestRequest('GET a', '60'),
+            TestRequest('COMMIT', None),
+            TestRequest('GET a', '60'),
+
+        ]
+
+        self._run_test(requests)
+
+    def test_transactions4(self):
+        requests = [
+            TestRequest('SET a 10', None),
+            TestRequest('BEGIN', None),
+            TestRequest('COUNT 10', 1),
+            TestRequest('BEGIN', None),
+            TestRequest('DELETE a', None),
+            TestRequest('COUNT 10', 0),
+            TestRequest('ROLLBACK', None),
+            TestRequest('COUNT 10', 1),
+        ]
